28cffbcccb72a45a079e3fa47718f9c9a37931bb
Luis Geniole <luis.geniole@gmail.com>
2019-12-26 14:44:00 -0300

    finish createTeam redux&saga

--- src/components/TeamSwitcher/index.js
+++ src/components/TeamSwitcher/index.js
@@ -16,6 +16,7 @@ class TeamSwitcher extends Component {
     selectTeam: PropTypes.func.isRequired,
     openTeamModal: PropTypes.func.isRequired,
     closeTeamModal: PropTypes.func.isRequired,
+    createTeamRequest: PropTypes.func.isRequired,
     teams: PropTypes.shape({
       data: PropTypes.arrayOf(
         PropTypes.shape({
@@ -25,6 +26,10 @@ class TeamSwitcher extends Component {
       ),
       teamModalOpen: PropTypes.bool.isRequired,
     }).isRequired,
+  };
+
+  state = {
+    newTeam: '',
   };
 
   componentDidMount() {
@@ -39,8 +44,22 @@ class TeamSwitcher extends Component {
     selectTeam(team);
   };
 
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleCreateTeam = e => {
+    e.preventDefault();
+
+    const { createTeamRequest } = this.props;
+    const { newTeam } = this.state;
+
+    createTeamRequest(newTeam);
+  };
+
   render() {
     const { teams, openTeamModal, closeTeamModal } = this.props;
+    const { newTeam } = this.state;
 
     return (
       <Container>
@@ -60,9 +79,13 @@ class TeamSwitcher extends Component {
             <Modal>
               <h1>Criar Time</h1>
 
-              <form onSubmit={() => {}}>
+              <form onSubmit={this.handleCreateTeam}>
                 <span>NOME</span>
-                <input name="newTeam" />
+                <input
+                  name="newTeam"
+                  value={newTeam}
+                  onChange={this.handleInputChange}
+                />
 
                 <Button size="big" type="submit">
                   Salvar
