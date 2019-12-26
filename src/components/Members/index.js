40f109df96faf3eaeb8efd03a3521185445a5f7e
Luis Geniole <luis.geniole@gmail.com>
2019-12-26 18:56:09 -0300

    add updateMemberRoles

--- src/components/Members/index.js
+++ src/components/Members/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Select from 'react-select';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+
+import api from '~/services/api';
 
 import MembersActions from '~/store/ducks/members';
 
@@ -15,16 +18,41 @@ class Members extends Component {
   static propTypes = {
     closeMembersModal: PropTypes.func.isRequired,
     getMembersRequest: PropTypes.func.isRequired,
+    updateMemberRequest: PropTypes.func.isRequired,
+    members: PropTypes.shape({
+      data: PropTypes.arrayOf({
+        id: PropTypes.number,
+        user: PropTypes.shape({
+          name: PropTypes.string,
+        }),
+        roles: PropTypes.array,
+      }),
+    }).isRequired,
   };
 
-  componentDidMount() {
+  state = {
+    roles: [],
+  };
+
+  async componentDidMount() {
     const { getMembersRequest } = this.props;
 
     getMembersRequest();
+
+    const response = await api.get('roles');
+
+    this.setState({ roles: response.data });
+  }
+
+  handleRoleChange(id, roles) {
+    const { updateMemberRequest } = this.props;
+
+    updateMemberRequest(id, roles);
   }
 
   render() {
     const { closeMembersModal, members } = this.props;
+    const { roles } = this.state;
 
     return (
       <Modal size="big">
@@ -35,6 +63,14 @@ class Members extends Component {
             {members.data.map(member => (
               <li key={member.id}>
                 <strong>{member.user.name}</strong>
+                <Select
+                  isMulti
+                  options={roles}
+                  value={member.roles}
+                  getOptionLabel={role => role.name}
+                  getOptionValue={role => role.id}
+                  onChange={value => this.handleRoleChange(member.id, value)}
+                />
               </li>
             ))}
           </MembersList>
--- src/store/sagas/members.js
+++ src/store/sagas/members.js
@@ -9,3 +9,31 @@ export function* getMembers() {
 
   yield put(MembersTypes.getMembersSuccess(response.data));
 }
+
+export function* updateMember({ id, roles }) {
+  try {
+    yield call(api.put, `members/${id}`, {
+      roles: roles.map(role => role.id),
+    });
+
+    yield put(
+      toastrActions.add({
+        type: 'success',
+        title: 'Membro atualizado!',
+        message: 'O membro foi ataulizado com sucesso.',
+        timeOut: 300,
+        progressBar: true,
+      })
+    );
+  } catch (err) {
+    yield put(
+      toastrActions.add({
+        type: 'error',
+        title: 'Erro na operação!',
+        message: 'Houve um erro, tente novamente.',
+        timeOut: 300,
+        progressBar: true,
+      })
+    );
+  }
+}
